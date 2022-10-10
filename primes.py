572a78676b5a3b4d783a0cdadee4b6ad83d161e1
Sabeeka Ahmad (k20012890) <sabeeka.1.ahmad@kcl.ac.uk>
2022-10-10 21:55:56 +0100

    Initial attempt

--- primes.py
+++ primes.py
@@ -1,6 +1,24 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
+
+def checkPrime(x):
+    if x == 2 or x == 3:
+        return True
+    for i in range(2, math.ceil(x/2)+1):
+        if x%i == 0:
+            return False
+    return True
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes < 1:
+        raise ValueError("Number must be greater than 0")
+    else:
+        list = []
+        x = 2
+        while len(list) < number_of_primes:
+            isPrime = checkPrime(x)
+            if isPrime:
+                list.append(x)
+            x += 1
+        return list
